643af3930ca1942d90cee556bedf3692628821f5
Alvyn_Abranches <alvynabranches@gmail.com>
2019-09-12 00:26:05 +0530

    Directory Changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('login.html');
+    require_once('views/login.html');
     if(empty($_POST['username']) || empty($_POST['password'])) {
         echo "<script type='javascript'>alert('Empty Parameters')</script>";
     }
@@ -7,6 +7,5 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
     }
-    require_once('config.php');
-    
+    require_once('php/config.php');
 ?>
