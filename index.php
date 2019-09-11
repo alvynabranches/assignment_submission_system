5305d9252cf640cf8dce731ac034f916323786c7
Alvyn Abranches <alvynabranches@gmail.com>
2019-09-11 20:22:20 +0530

    index.php updated

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
     require_once('views/student.html');
+    if($_POST['username'] === '' || $_POST['password'] === '') {
+        echo "<script type='javascript'>alert('Empty Parameters')</script>";
+    }
+    else {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
 ?>
